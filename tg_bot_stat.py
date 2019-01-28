67bc96177d279892952a4afcb7d21dc284eca0b5
shoker444 <shoker444@gmail.com>
2019-01-28 23:55:03 +0300

    Cloudcube support

--- tg_bot_stat.py
+++ tg_bot_stat.py
@@ -14,8 +14,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 session = boto3.session.Session()
 s3 = session.client(
-    service_name='s3',
-    endpoint_url='https://storage.yandexcloud.net'
+    service_name='s3'
 )
 bot = telebot.TeleBot("787378414:AAGuzZDHyCEJY7ssd0LP_76HaDZ-oRekF2k")
 inst_usr = os.environ['INST_USER']
@@ -25,8 +24,8 @@ link1 = ""
 admin_id = os.environ['ADMIN_TG_ID']
 tg_channel = os.environ['TG_CHANNEL']
 try:
-    for key in s3.list_objects(Bucket='heroku')['Contents']:
-        s3.download_file('heroku', key['Key'], key['Key'])
+    for key in s3.list_objects(Bucket='cloud-cube-eu')['Contents']:
+        s3.download_file('cloud-cube-eu', key['Key'], key['Key'])
 except: print("Error appeared")
 @bot.message_handler(func=lambda message: True)
 def message_receive(message):
@@ -84,7 +83,6 @@ def send_inst_status(message):
 if (os.path.isfile("working.ase")): 
     os.remove("working.ase")
     print("file removed")
-tg_bot = subprocess.Popen("python tg_bot.py "+admin_id+" "+tg_channel, shell=True, preexec_fn=os.setsid)
+#tg_bot = subprocess.Popen("python tg_bot.py "+admin_id+" "+tg_channel, shell=True, preexec_fn=os.setsid)
 inst_bot = subprocess.Popen("python inst_bot.py "+inst_usr+" "+inst_pwd, shell=True, preexec_fn=os.setsid)
 bot.infinity_polling(True)
-os.killpg(os.getpgid(tg_bot.pid), signal.SIGTERM)
