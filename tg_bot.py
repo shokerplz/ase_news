c40b1599f5885edf09df9dcf7dd2309ff0a35279
Ivan Kovalev <admin@Ivans-MacBook-Pro.local>
2019-02-06 15:24:49 +0300

    Deleted some resources

--- tg_bot.py
+++ tg_bot.py
@@ -16,39 +16,18 @@ s3 = session.client(
     service_name='s3',
     endpoint_url='https://storage.yandexcloud.net'
 )
-def start():
-        if len(sys.argv)<3:
-                print("You have not wrote your chat id and channel id")
-                print("This bot must be called with tg_bot.py <Your dialog id> <Channel id>")
-                sys.exit()
-        try:
-                check_file()
-        except:
-                print("Error handled thanks to coder")
-                pass
 def check_file():
     global past_link
     f = open("links.txt", "a+")
     past_link = f.read()
     print(past_link)
-    while (len(past_link) == 0): 
-        time.sleep(1)
-        past_link = f.read()
-    if (len(past_link) != 0):
-        print(len(past_link))
-        check_site(past_link)
+    check_site(past_link)
 def check_site(past_link):
         global links
         global work
         f = open("links.txt", "a+")
         data = f.read()
         while True:
-            #work = open("working.ase", "a+")
-            #work_file = work.read()
-            #if (work_file == ""):
-             #   work.write("1")
-              #  work.close()
-            sys.stdout.flush()
             if (data != past_link):
                 open("links.txt", "w").close()
                 f.write(past_link)
@@ -61,7 +40,6 @@ def check_site(past_link):
             for link in soup.find_all('a', href=True):
                 links.append(link.get('href'))
             if (past_link != links[0]):
-                #bot_send(links[0])
                 past_link = links[0]
             time.sleep(30)
             del links[:]
@@ -73,4 +51,4 @@ def bot_send(message):
         print(message)
         url_html = "<a href='"+message+"'>"+soup1.title.string+"</a> "+ pc_link
         bot.send_message(sys.argv[2], url_html, parse_mode = 'HTML')
-start()
+check_file()
