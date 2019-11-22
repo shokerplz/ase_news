4cd7f6b2b01e907ec64adf3d85a9d98072547006
Ivan Kovalev <shoker444@gmail.com>
2019-11-23 00:09:50 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -74,6 +74,7 @@ def bot_send(message):
                 name = soup1.title.string[:-18]
             else: name = soup1.title.string
             url_html = "<a href='"+message+"'>"+name+"</a> "+"\n"+pc_link
+            print(url_html)
             try:
                 bot.send_message(sys.argv[2], url_html, parse_mode = 'HTML')
             except Exception as e:
